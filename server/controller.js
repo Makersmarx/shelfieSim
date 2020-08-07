fee951e4c736ac766e554c7cd6258f33381e9f3b
makers <bbiehn1@gmail.com>
2020-08-07 15:38:04 -0600

    random practice with Shelfie

---
+++ server/controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+	read: (req, res, next) => {
+		const dbInstance = req.app.get('db');
+
+		dbInstance.get_inventory().then((products) => res.status(200).send(products)).catch((err) => {
+			res.status(500).send({ errorMessage: 'Its Not Right!' });
+			console.log(err);
+		});
+	}
+	
---
+++ server/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express = require('express')
+
+// Massive
+const massive = require('massive')
+
+const app = express()
+
+//controller
+const controller = require('./controller')
+
+// Port being used
+const port = 8000
+
+// Destructing vars from .env to use in index.js
+const {
+    SERVER_PORT,
+    CONNECTION_STRING
+} = process.env
+
+// Using massive to connect to db
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+.then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+.catch((err) => console.log(err))
+
+
+
+
+
+app.use(express.json())
+
+// Endpoints
+app.get('/api/inventory', controller.read)
+
+
+
+
+app.listen(port, () => console.log(`We hear you at Port: ${port}`))
